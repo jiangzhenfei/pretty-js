731f68f5d7fd32ed93dfbb4cd736cd5f27dd6d72
江振飞 <zhenfei_jiang@dnt.com.cn>
2018-09-11 16:58:56 +0800

    promise实现

--- js/index.js
+++ js/index.js
@@ -121,8 +121,64 @@ var name = 'fei'
 function People(name){
     this.name = name
 }
+var _self = this;
 People.prototype.say =()=>{
-    console.log(this.name)
+    console.log(_self.name)
 }
 var p = new People('jiao')
 p.say()//打印是啥
+
+/**
+ * 实现简易的promise
+ * 为了简便值考虑resolve
+ */
+
+ function Promise(){
+    var state = 'pendding',//状态初始化
+        value = null,//resolve时候的值将会保存在这里
+        callbacks = []//resolve之后要执行的回调
+
+    this.then = function( func ){
+        return new Promise( function( resolve ){
+            handle( { 
+                onFulfilled: func || null,
+                resolve:     resolve
+            } )
+        } )
+    }
+    function handle(callback) {
+        if (state === 'pending') {//pedding状态往数组添加回调
+            callbacks.push(callback);
+            return;
+        }
+        //如果then中没有传递任何东西
+        if(!callback.onFulfilled) {
+            callback.resolve(value);
+            return;
+        }
+        var ret = callback.onFulfilled(value);//执行当前的.then内部的函数
+        callback.resolve(ret);//获取的结果触发下一个promise的resolve
+    }
+    function resolve(newValue) {
+        //如果返回的是一个promise（1），那么当前的resolve不会循环执行callbacks，
+        //而是往该promise内部。then一个回调，回调就是当前promise正常的resolve，
+        //当promise（1）成功，触发当前的resolve，也就触发该ppromise内部的callbacks
+        if (newValue && (typeof newValue === 'object' || typeof newValue === 'function')) {
+            var then = newValue.then;
+            if (typeof then === 'function') {
+                then.call(newValue, resolve);
+                return;
+            }
+        }
+        state = 'fulfilled';
+        value = newValue;
+        setTimeout(function () {
+            callbacks.forEach(function (callback) {
+                handle(callback);
+            });
+        }, 0);
+    }
+
+    fn(resolve);
+
+ }
